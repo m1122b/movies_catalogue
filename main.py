adca51a859fc6740ba4c7a11dfbdac743fe550e1
Miroslaw Biernacki <mb100773@gmail.com>
2022-03-17 22:08:36 +0100

    Add movie datails, but not all

--- main.py
+++ main.py
@@ -27,6 +27,24 @@ def utility_processor():
     return {"tmdb_image_url": tmdb_image_url}
 
 
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    print(details)
+    how_many = 4
+    cast = tmdb_client.get_single_movie_cast(movie_id)[:how_many]
+    print(cast)
+    return render_template("movie_details.html", movie=details, cast=cast)
+
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_backdrop(path, size):
+        return tmdb_client.get_backdrop_path(path, size)
+    return {"tmdb_image_backdrop": tmdb_image_backdrop}
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- tmdb_client.py
+++ tmdb_client.py
@@ -3,10 +3,12 @@ from random import choices
 import requests
 
 
+PAYLOAD = {'api_key': 'de232542034b3603e0d8b59a43a78c8f', 'language': 'en-US', 'page': '1'}
+
+
 def get_popular_movies():
-    payload = {'api_key': 'de232542034b3603e0d8b59a43a78c8f', 'language': 'en-US', 'page': '1'}
     url = 'https://api.themoviedb.org/3/movie/popular'
-    r = requests.get(url, params=payload)
+    r = requests.get(url, params=PAYLOAD)
     return r.json()
 
 
@@ -20,6 +22,24 @@ def get_movies(how_many):
     return data
 
 
+def get_single_movie(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    r = requests.get(url, params=PAYLOAD)
+    return r.json()
+
+
+def get_backdrop_path(backdrop_api_path, size = 'w780'):
+    base_url = 'https://image.tmdb.org/t/p/'
+    return f"{base_url}{size}{backdrop_api_path}"
+
+
+def get_single_movie_cast(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    r = requests.get(url, params=PAYLOAD)
+    return r.json()['cast']
+
+
+
 if __name__ == '__main__':
     
     data = get_popular_movies()
@@ -30,6 +50,12 @@ if __name__ == '__main__':
 
     path = get_poster_url('/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg')
     print(path)
+
+    print(get_backdrop_path('/dK12GIdhGP6NPGFssK2Fh265jyr.jpg'))
+
+    print("/")
+
+    print(get_single_movie_cast(928999))
     
 
 
